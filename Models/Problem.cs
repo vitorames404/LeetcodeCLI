d9990e05a4349a05b125c26db43d041b4410dd19
vitorames404 <vitorames@protonmail.com>
2025-05-21 19:44:03 +1200

    First commit

---
+++ Models/Problem.cs
@@ -0,0 +1,15 @@
+namespace LeetCode.Models
+{
+  public class Problem
+  {
+    public int Id{get; set;}
+    public string Title{get; set;}
+    public string Url{get; set;}
+    public string Difficulty{get; set;}
+    public string Topic{get; set;}
+    public string Status{get; set;}
+    public DateTime? SolvedAt{get; set;}
+  }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+while (true)
+{
+    Console.WriteLine("\nWelcome to LeetCLI");
+    Console.WriteLine("1. Show next problem");
+    Console.WriteLine("2. Mark problem as solved");
+    Console.WriteLine("3. Skip problem");
+    Console.WriteLine("4. View progress");
+    Console.WriteLine("5. Exit");
+
+    Console.Write("Choose an option: ");
+    var input = Console.ReadLine();
+
+    Console.WriteLine("Teste porr");
+
+}
